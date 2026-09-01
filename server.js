31fd67343f5e7518f7c73aa3cc536b2bf7104beb
tclocker32 <tclocker32@gmail.com>
2026-09-01 09:46:59 -0700

    Fix response to send index.html file

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.post("/stock-review", (req, res) => {
 app.post("/stock-review", (req, res) => {
 });
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  path.join(__dirname, "public", "index.html")
 });
 app.listen(PORT, () => console.log(`Turill Financial V2 running at http://localhost:${PORT}`));
